4408a154c3032290ced1b07697d296dcbf0049d3
Kumina <redgoat98@gmail.com>
2023-02-09 00:18:02 +0200

    Changed edit url in the consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -19,7 +19,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+export const GITHUB_EDIT_URL = `https://github.com/kumina-dev/opposite-orbit/tree/master`;
 
 export const COMMUNITY_INVITE_URL = `https://discord.kumina.wtf`;
 
